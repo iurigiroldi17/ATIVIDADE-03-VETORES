190dd4e160ed026e7be53fe475277ce75666f488
iurigiroldi17 <iuri241206@edu.unipar.br>
2024-09-04 20:34:19 -0300

    ATIVIDADE 03 VETORES

---
+++ src/InicializaçãoeAcessoaElementosdoVetor.java
@@ -0,0 +1,11 @@
+public class InicializaçãoeAcessoaElementosdoVetor {
+
+    public static void main(String[] args) {
+
+        int[] vetor = {10, 20, 30, 40, 50};
+
+        int terceiroElemento = vetor[2];
+
+        System.out.println("O terceiro elemento do vetor é: " + terceiroElemento);
+    }
+}
