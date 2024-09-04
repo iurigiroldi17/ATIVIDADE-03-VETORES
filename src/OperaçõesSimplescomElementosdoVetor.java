a1db498965cd4c05dfe871978b202a1fe9ed29da
iurigiroldi17 <iuri241206@edu.unipar.br>
2024-09-04 20:50:30 -0300

    ATIVIDADE 03 VETORES

---
+++ src/OperaçõesSimplescomElementosdoVetor.java
@@ -0,0 +1,13 @@
+public class OperaçõesSimplescomElementosdoVetor {
+    public static void main(String[] args) {
+
+        int[] vetor = {3, 6, 5, 12};
+
+        int primeiroElemento = vetor[0];
+        int ultimoElemento = vetor[1];
+
+        int soma = primeiroElemento + ultimoElemento;
+
+        System.out.println("A soma do primeiro e do ultimo elemento: " + soma);
+    }
+}
